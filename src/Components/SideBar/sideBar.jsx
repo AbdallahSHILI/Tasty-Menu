ef55f860afd3446ceec876b996d2dbfb53b63867
Abdallah <shiliabdallah1234@gmail.com>
2024-11-09 13:10:28 +0100

    Add HomeIcon to SideBar and adjust styles

--- src/Components/SideBar/sideBar.jsx
+++ src/Components/SideBar/sideBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./sideBar.module.css";
 import { menuData } from "../../data/menuData";
+import HomeIcon from "../../Components/Assets/HomeIcon.svg";
 
 const categoryRoutes = {
   1: "/crepe",
@@ -17,6 +18,12 @@ const categoryRoutes = {
 const SideBar = ({ isOpen }) => {
   return (
     <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+      <Link to="/" className={`${styles.categoryItem} ${styles.homeItem}`}>
+        <div className={styles.homeName}>
+          <img src={HomeIcon} alt="Home" className={styles.homeIcon} />
+          <span className={styles.categoryText}>Accueil</span>
+        </div>
+      </Link>
       {Object.entries(menuData).map(([id, category]) => (
         <Link
           key={id}

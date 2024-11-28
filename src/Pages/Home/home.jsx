b0003014581faf6ba2c8147d3cd31a5bd2c82988
Abdallah <shiliabdallah1234@gmail.com>
2024-11-28 12:19:48 +0100

    Add Supplement component, enhance Menu with subcategory handling

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./home.module.css";
-import { HomeHeader, FoodCategory, Menu } from "../index";
+import { HomeHeader, FoodCategory, Menu, Supplement } from "../index";
 
 const Home = () => {
+  const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+  const [categoryId, setCategoryId] = useState(null);
+
   return (
     <div className={styles.container}>
       <HomeHeader />
       <FoodCategory />
-      <Menu />
+      <Menu
+        onSubcategoryChange={(subcategory, category) => {
+          setSelectedSubcategory(subcategory);
+          setCategoryId(category);
+        }}
+      />
+      {selectedSubcategory && categoryId && (
+        <Supplement subcategory={selectedSubcategory} categoryId={categoryId} />
+      )}
     </div>
   );
 };
